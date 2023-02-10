9a5e05136b4df83a3d8ac30295636f1f52d076ee
Rasmus Nørby <52028666+r4sNorby@users.noreply.github.com>
2023-02-10 12:10:31 +0100

    Added HelloWorld via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minVar = 500;
+            int wn;
+
+            Console.WriteLine("Indtast et tal");
+            minVar = int.Parse(Console.ReadLine());
+            for(int n = 0; n <= minVar; n++)
+            {
+                Console.Write("*");
+            }
+            Console.ReadLine();
+
+            wn = 0;
+            while (wn < minVar)
+            {
+                Console.Write("+");
+                wn++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
